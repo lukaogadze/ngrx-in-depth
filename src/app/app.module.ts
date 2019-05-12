4aea351e970a15673bc2b494bc0922e28d8cc460
Luka Oghadze <loghadze@justice.gov.ge>
2019-05-12 17:43:25 +0400

    completed module "4. Essential NgRx Development Tools - NgRx Store Freeze and NgRx Router Store"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,23 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 
-import {
-    MatButtonModule,
-    MatListModule,
-    MatSidenavModule, MatToolbarModule,
-} from '@angular/material';
-import {HttpClientModule} from "@angular/common/http";
+import {MatButtonModule, MatListModule, MatSidenavModule, MatToolbarModule,} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 
-import {RouterModule, Routes} from "@angular/router";
-import {AuthModule} from "./auth/auth.module";
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthModule} from './auth/auth.module';
+import {StoreModule} from '@ngrx/store';
+import {metaReducers, reducers} from './reducers';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 import {AuthGuard} from './auth/auth.guard';
 import {EffectsModule} from '@ngrx/effects';
+import {NavigationActionTiming, StoreRouterConnectingModule} from '@ngrx/router-store';
+import {CustomSerializer} from './shared/custom-route-serializer';
 
 
 const routes: Routes = [
@@ -55,6 +53,11 @@ const routes: Routes = [
         StoreModule.forRoot(reducers, { metaReducers }),
         EffectsModule.forRoot([]),
         !environment.production ? StoreDevtoolsModule.instrument() : [],
+        StoreRouterConnectingModule.forRoot({
+            stateKey: "router",
+            serializer: CustomSerializer,
+            navigationActionTiming: NavigationActionTiming.PostActivation
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -7,11 +7,13 @@ import {
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import {storeFreeze} from 'ngrx-store-freeze';
+import {routerReducer} from '@ngrx/router-store';
 
 export interface AppState {
 }
 
 export const reducers: ActionReducerMap<AppState> = {
+    router: routerReducer
 };
 
 
